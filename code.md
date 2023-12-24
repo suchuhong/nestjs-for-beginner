2bf3bddb551256c7beef73be4082b069942f6d91
suchuhong <1442615457@qq.com>
2023-12-24 14:42:48 +0800

    lesson04 pipe、校验、抛出异常

--- code.md
+++ code.md
@@ -1,9 +1,11 @@
-## 1
+## 1、基本概念
 
 npm i -g @nestjs/cli
 nest new nestjs-for-beginner
 
-## 2
+https://nest.nodejs.cn/
+
+## 2、创建 module、service、controller
 
 nest g module users
 nest g service users
@@ -12,4 +14,18 @@ nest g controller users
 https://docs.nestjs.com/first-steps
 https://github.com/gitdagray/nestjs-course
 
-## 3
+https://docs.nestjs.com/controllers
+
+## 3、认识 providers
+
+https://docs.nestjs.com/providers
+
+## 4、认识pipe、校验、抛出异常
+
+npm install @nestjs/mapped-type
+npm install class-validator
+npm i class-transformer
+
+https://docs.nestjs.com/pipes
+https://github.com/typestack/class-validator#validation-decorators
+https://docs.nestjs.com/techniques/validation
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -37,22 +39,21 @@ export class UsersService {
 
   findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     if (role) {
-      return this.users.filter((user) => user.role === role);
+      const rolesArray = this.users.filter((user) => user.role === role);
+      if (rolesArray.length === 0)
+        throw new NotFoundException('User Role Not Found');
+      return rolesArray;
     }
     return this.users;
   }
 
   findOne(id: number) {
     const user = this.users.find((user) => user.id === id);
-
+    if (!user) throw new NotFoundException('User Not Found');
     return user;
   }
 
-  create(user: {
-    name: string;
-    email: string;
-    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
-  }) {
+  create(user: CreateUserDto) {
     const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
     const newUser = {
       id: usersByHighestId[0].id + 1,
@@ -62,14 +63,7 @@ export class UsersService {
     return newUser;
   }
 
-  update(
-    id: number,
-    updatedUser: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
-  ) {
+  update(id: number, updatedUser: UpdateUserDto) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
         return { ...user, ...updatedUser };
